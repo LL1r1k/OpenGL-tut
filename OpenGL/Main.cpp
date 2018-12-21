a1c9ad6452a8fd4280037860a696cb4faa56f0f8
LL1r1k <kks1999.12.24@mail.ru>
2018-12-21 17:07:17 +0400

    Multiple texture

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -205,6 +205,7 @@ int main(int args, char** argv)
 	glBindVertexArray(0);
 
 	//TEXUTE INIT
+	//Texture 0
 	int image_width = 0, image_height = 0;
 	unsigned char* image = SOIL_load_image("Images/mario.png", &image_width, &image_height, NULL, SOIL_LOAD_RGBA);
 
@@ -231,6 +232,33 @@ int main(int args, char** argv)
 	glBindTexture(GL_TEXTURE_2D, 0);
 	SOIL_free_image_data(image);
 
+	//Texture 1
+	int image_width1 = 0, image_height1 = 0;
+	unsigned char* image1 = SOIL_load_image("Images/pusheen.png", &image_width1, &image_height1, NULL, SOIL_LOAD_RGBA);
+
+	GLuint texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	if (image1)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width1, image_height1, 0, GL_RGBA, GL_UNSIGNED_BYTE, image1);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "ERROR:TEXTURE_LOADING_FAILED" << std::endl;
+	}
+
+	glActiveTexture(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image1);
+
 	//--MAIN LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -249,10 +277,13 @@ int main(int args, char** argv)
 		
 			//Update uniform
 		glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
+		glUniform1i(glGetUniformLocation(core_program, "texture1"), 1);
 
 			//Activate texture
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture0);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture1);
 			//Bind vertex array object
 		glBindVertexArray(VAO);
 			
