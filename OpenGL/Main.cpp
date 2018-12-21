eda9926ba42771883fe571c02b23d12283d6fd50
LL1r1k <kks1999.12.24@mail.ru>
2018-12-21 17:23:34 +0400

    ModelMatrix

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -259,6 +259,15 @@ int main(int args, char** argv)
 	glBindTexture(GL_TEXTURE_2D, 0);
 	SOIL_free_image_data(image1);
 
+	glm::mat4 ModelMatrix(1.f);
+	ModelMatrix = glm::translate(ModelMatrix, glm::vec3(0.f, 0.f, 0.f)); // add 
+	ModelMatrix = glm::rotate(ModelMatrix, glm::radians(0.f), glm::vec3(1.f, 0.f, 0.f)); //x axis
+	ModelMatrix = glm::scale(ModelMatrix, glm::vec3(1.f)); 
+
+	glUseProgram(core_program);
+	glUniformMatrix4fv(glGetUniformLocation(core_program, "ModelMatrix"), 1, GL_FALSE, glm::value_ptr(ModelMatrix));
+	glUseProgram(0);
+	
 	//--MAIN LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -278,6 +287,13 @@ int main(int args, char** argv)
 			//Update uniform
 		glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
 		glUniform1i(glGetUniformLocation(core_program, "texture1"), 1);
+
+		//Move, rotate and scale 
+		ModelMatrix = glm::translate(ModelMatrix, glm::vec3(0.f, 0.f, 0.f)); // add 
+		ModelMatrix = glm::rotate(ModelMatrix, glm::radians(5.f), glm::vec3(0.f, 0.f, 1.f)); //x axis
+		ModelMatrix = glm::scale(ModelMatrix, glm::vec3(1.001f));
+
+		glUniformMatrix4fv(glGetUniformLocation(core_program, "ModelMatrix"), 1, GL_FALSE, glm::value_ptr(ModelMatrix));
 
 			//Activate texture
 		glActiveTexture(GL_TEXTURE0);
