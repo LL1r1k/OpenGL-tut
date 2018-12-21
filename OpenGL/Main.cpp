ec4c2de2ec2d910de08718f08bc3b5b76123084c
LL1r1k <kks1999.12.24@mail.ru>
2018-12-21 16:08:05 +0400

    Render a QUAD using an index array

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -1,15 +1,18 @@
 #include "lib.h"
 
 Vertex vertices[] = {
-	glm::vec3(0.0f, 0.5f, 0.f), glm::vec3(1.f, 0.f, 0.f), glm::vec2(0.f, 1.f),
-	glm::vec3(-0.5f, -0.5f, 0.f), glm::vec3(0.f, 1.f, 0.f), glm::vec2(0.f, 0.f),
-	glm::vec3(0.5f, -0.5f, 0.f), glm::vec3(0.f, 0.f, 1.f), glm::vec2(1.f, 0.f)
+	//Position						Color						Texcoord
+	glm::vec3(-0.5f, 0.5f, 0.f),	glm::vec3(1.f, 0.f, 0.f),	glm::vec2(0.f, 1.f),
+	glm::vec3(-0.5f, -0.5f, 0.f),	glm::vec3(0.f, 1.f, 0.f),	glm::vec2(0.f, 0.f),
+	glm::vec3(0.5f, -0.5f, 0.f),	glm::vec3(0.f, 0.f, 1.f),	glm::vec2(1.f, 0.f),
+	glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f, 1.f, 0.f),	glm::vec2(0.f, 0.f)	
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
 GLuint indices[]=
 {
-	0, 1, 2
+	0, 1, 2, // Triangle 1
+	0, 2, 3 // Trinagle 2
 };
 unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
 
