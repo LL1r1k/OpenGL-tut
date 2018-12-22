03fde5a0706fe86797e90de285ed303efc37096c
LL1r1k <kks1999.12.24@mail.ru>
2018-12-22 12:28:06 +0400

    Func in fragShader

--- OpenGL/fragment_core.glsl
+++ OpenGL/fragment_core.glsl
@@ -17,33 +17,33 @@ in vec3 vs_normal;
 out vec4 fs_color;
 
 uniform Material material;
-
-uniform sampler2D texture0;
-
 uniform vec3 lightPos0;
 uniform vec3 cameraPos;
 
-void main()
+vec3 calcAmbient()
 {
-	//Ambient light
-	vec3 ambientLight = material.ambient;
+	return material.ambient;
+}
 
-	//Defuse light
+vec3 calcDiffuse()
+{
 	vec3 posToLightDirVec = normalize(vs_position - lightPos0);
 	float diffuse = clamp(dot(posToLightDirVec, vs_normal), 0, 1);
-	vec3 diffuseFinal =  material.diffuse * diffuse;
+	return material.diffuse * diffuse;
+}
 
-	//Specular light
+vec3 calcSpecular()
+{
 	vec3 lightToPosDirVec = normalize(lightPos0 - vs_position);
 	vec3 reflectDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
 	vec3 posToViewDirVec = normalize(vs_position - cameraPos);
 	float specularConstant = pow(max(dot(posToViewDirVec, reflectDirVec), 0), 30);
-	vec3 specularFinal = material.specular * specularConstant;
-
+	return material.specular * specularConstant;
+}
+void main()
+{
 	//Attenuation
 
-	//Final light
-
-	fs_color = texture(material.diffuseTex, vs_texcoord)*
-	(vec4(ambientLight, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
+	fs_color = texture(material.diffuseTex, vs_texcoord) *
+	(vec4(calcAmbient(), 1.f) + vec4(calcDiffuse(), 1.f) + vec4(calcSpecular(), 1.f));
 }
